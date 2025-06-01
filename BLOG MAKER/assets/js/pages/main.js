0b0de74bb3c9960723771cb9c3ab7a93f62951e1
carlo-baron <carlobaron430@gmail.com>
2025-06-01 15:49:13 +0800

    removed codes that needs rework

--- BLOG MAKER/assets/js/pages/main.js
+++ BLOG MAKER/assets/js/pages/main.js
@@ -8,37 +8,12 @@ document.addEventListener("click", (event) => {
     if (target.classList.contains("like-btn")) {
         event.preventDefault();
         console.log(`Liked post ${postId}`);
-        handleInteraction("like", postId);
     } else if (target.classList.contains("comment-btn")) {
         event.preventDefault();
         console.log(`Commented post ${postId}`);
-        handleInteraction("comment", postId);
     } else if (target.classList.contains("share-btn")) {
         event.preventDefault();
         console.log(`Shared post ${postId}`);
-        handleInteraction("share", postId);
     }
 });
 
-function handleInteraction(action, postId) {
-    fetch("comment-post.php", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ action: action, post_id: postId })
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.success) {
-                if (action === "comment") {
-                    window.location.href = "/BLOG%20MAKER/open-post.php";
-                } else {
-                    console.log(`${action} action succeeded for post ${postId}`);
-                }
-            } else {
-                console.error(`Error with ${action} action:`, data.error);
-            }
-        })
-        .catch((error) => {
-            console.error("Fetch error:", error);
-        });
-}
