84067aa32669b8a6e432a64a43b84cff4438d03a
carlo-baron <carlobaron430@gmail.com>
2025-06-15 13:33:49 +0800

    Login.php Security update

--- blog-maker/src/auth/login.php
+++ blog-maker/src/auth/login.php
@@ -6,25 +6,31 @@
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $sql_get_username = "SELECT * FROM login WHERE username = '$username'";
-        $result = mysqli_query($connection, $sql_get_username);
+        $stmt = $connection->prepare("SELECT * FROM login WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
             $verify_password = password_verify($password, $row['password']);
 
             if ($verify_password) {
                 $loginId = $row["id"];
-                $sql = "SELECT user_id FROM users WHERE login_id = '$loginId'";
-                $userResult = mysqli_query($connection, $sql);
-                if(mysqli_num_rows($userResult) > 0){
-                    $row = mysqli_fetch_assoc($userResult);
+
+                $stmt = $connection->prepare("SELECT user_id FROM users WHERE login_id = ?");
+                $stmt->bind_param("i", $loginId);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if($result->num_rows > 0){
+                    $row = $result->fetch_assoc();
                     $_SESSION["id"] = $row["user_id"];
+                    session_regenerate_id(true);
                 }
                 header("Location: ../../src/pages/main.php");
                 exit();
             } else {
-                echo "incorrect password";
                 header("Location: ../../index.php");
                 exit();
             }
@@ -34,5 +40,5 @@
         }
     }
 
-    mysqli_close($connection);
+    $connection->close();
 ?>
